e45f6647357fd0ae789228b206e13178ec807709
Ke Xu <kxu12348760@gmail.com>
2013-04-28 22:20:56 -0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SampleApp::Application.routes.draw do
+  resources :users
+  
   root :to => "static_pages#home"
   match "/signup" => "users#new"
   match "/about" => "static_pages#about"
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name	"Ke Xu"
+		email	"kxu12348760@gmail.com"
+		password	"foobar"
+		password_confirmation	"foobar"
+	end
+end
