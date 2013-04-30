77b4287c184347954ceb7a1a878061b58a5629fa
Ke Xu <kxu12348760@gmail.com>
2013-04-30 00:16:04 -0700

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   
   root :to => "static_pages#home"
+
   match "/signup" => "users#new"
+  match "/signin" => "sessions#new"
+  match "/signout" => "sessions#destroy", via: :delete
   match "/about" => "static_pages#about"
   match "/help" => "static_pages#help"
   match "/contact" => "static_pages#contact"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,13 +13,14 @@
 require 'spec_helper'
 
 describe User do
-	before { @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")}
+	before { @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar") }
 	subject { @user }
 	it { should respond_to(:name) }
 	it { should respond_to(:email) }
 	it { should respond_to(:password_digest) }
 	it { should respond_to(:password_confirmation) }
 	it { should respond_to(:authenticate) }
+	it { should respond_to(:remember_token) }
 	it { should be_valid }
 
 	describe "when name is not present" do
@@ -111,5 +112,10 @@ describe User do
 			@user.save
 			@user.reload.email.should == mixed_case_email.downcase
 		end
+	end
+
+	describe "remember token" do
+		before { @user.save }
+		its(:remember_token) { should_not be_blank }
 	end
 end
